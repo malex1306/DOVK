399fd6f36049e3e25f9a4eeca6c26cec3f6cf4e8
vascoalexander <vascoalexander@gmail.com>
2024-06-21 11:28:54 +0200

    converter and data

---
+++ converter.py
@@ -0,0 +1,33 @@
+from docx import Document
+import os
+
+folder_path = 'data/'
+qa_dict = {}
+
+def read_questions_answers(filename):
+    doc = Document(filename)
+    qa_dict = {}
+    current_question = None
+
+    for para in doc.paragraphs:
+        text = para.text.strip()
+        if text.endswith("?"):
+            # Es ist eine Frage
+            current_question = text
+            qa_dict[current_question] = ""
+        elif current_question:
+            # Es ist eine Antwort zu der zuletzt gefundenen Frage
+            qa_dict[current_question] += text + "\n"
+
+    # Entferne den letzten Zeilenumbruch von jeder Antwort
+    for question in qa_dict:
+        qa_dict[question] = qa_dict[question].strip()
+
+    return qa_dict
+
+# Iteration über alle Dateien in folder_path
+for file in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, file)
+    if os.path.isfile(file_path):
+        current_file = read_questions_answers(file_path)
+        qa_dict.update(current_file)
